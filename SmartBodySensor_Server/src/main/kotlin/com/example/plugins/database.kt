3ea73ea337eecf7e2eb6dcc6f2085e45514de5dc
naegerob <naeger.robert@gmail.com>
2023-06-26 18:27:09 +0200

    Update database.kt

--- SmartBodySensor_Server/src/main/kotlin/com/example/plugins/database.kt
+++ SmartBodySensor_Server/src/main/kotlin/com/example/plugins/database.kt
@@ -1,10 +1,12 @@
 package com.example.plugins
 
 import org.jetbrains.exposed.sql.Table
+
 object JsonEntries : Table() {
     val id = integer("id").autoIncrement()
-    val title = varchar("title", 128)
-    val body = varchar("body", 1024)
+    val index = varchar("index", 128)
+    val tempDifference = varchar("title", 128)
+    val voltage = varchar("body", 1024)
 
     override val primaryKey = PrimaryKey(id)
 }
