0a9b8f00d5fdd26cd6fffebf58e7e48cf77be13b
naegerob <naeger.robert@gmail.com>
2023-06-30 17:40:34 +0200

    implementation started of database

---
+++ SmartBodySensor_Server/src/main/kotlin/com/example/database/JsonEntries.kt.kt
@@ -0,0 +1,12 @@
+package com.example.database
+
+import org.jetbrains.exposed.sql.Table
+
+object `JsonEntries.kt` : Table() {
+    private val id = integer("id").autoIncrement()
+    private val index = varchar("index", 128)
+    private val tempDifference = varchar("tempDifference", 128)
+    private val voltage = varchar("voltage", 128)
+
+    override val primaryKey = PrimaryKey(id)
+}
--- SmartBodySensor_Server/src/main/kotlin/com/example/database/JsonRepresenter.kt
+++ SmartBodySensor_Server/src/main/kotlin/com/example/database/JsonRepresenter.kt
@@ -1,4 +1,4 @@
-package com.example.plugins
+package com.example.database
 
 data class JsonEntry(
     val index: Int,
